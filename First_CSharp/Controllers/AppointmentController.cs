69ac9dbe5e9cfc3bd66caf4ce05185d7a8988177
filipe <filipe11402@outlook.pt>
2021-06-19 22:35:29 +0100

    initial project structure

---
+++ First_CSharp/Controllers/AppointmentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace First_CSharp.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id) 
+        {
+            return Ok($"You have entered the id {id}");
+        }
+    }
+}
